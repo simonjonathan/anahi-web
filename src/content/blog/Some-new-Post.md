93bb15d2f3fe353051ff787808b94df5f3b04f53
Jonathan Simon <jonathansi@gmx.de>
2023-10-02 18:58:22 +0200

    initial commit - setup astro with tina cms

---
+++ src/content/blog/Some-new-Post.md
@@ -0,0 +1,9 @@
+---
+title: Some new Post
+description: Hello is a description
+pubDatetime: 2023-10-01T22:00:00.000Z
+---
+
+Something new
+
+![](/astropaper-og.jpg)
---
+++ tina/config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true
+          },
+          {
+            type: "datetime",
+            name: "pubDatetime",
+            label: "Publish time",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Post Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
