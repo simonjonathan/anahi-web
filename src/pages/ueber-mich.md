93f29e36811dbbfede5bdb453b539f7972a35fa0
Jonathan Simon <jonathansi@gmx.de>
2023-10-03 21:25:59 +0200

    feat(content): germanify blog, strip tags page - fix some errors when no posts are present

---
+++ src/pages/ueber-mich.md
@@ -0,0 +1,8 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "Über mich"
+---
+
+Wer bin ich?
+
+Kontaktiere mich gerne via [email](mailto:anahi.montes@web.de).
--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,12 +1,15 @@
 import type { CollectionEntry } from "astro:content";
+import post from "./og-templates/post";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) =>
-  posts
+const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
+  if (!posts) return [];
+  return posts
     .filter(({ data }) => !data.draft)
     .sort(
       (a, b) =>
         Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
         Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
     );
+};
 
 export default getSortedPosts;
